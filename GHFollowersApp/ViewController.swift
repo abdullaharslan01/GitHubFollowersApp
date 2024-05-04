8184cc9db384621a5d5e082af16cf50ffcbbd612
abdullaharslan01 <abdullaharslan1473@gmail.com>
2024-05-04 15:24:21 +0300

    Initial Commit

---
+++ GHFollowersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowersApp
+//
+//  Created by abdullah on 4.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
